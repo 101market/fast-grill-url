39e3d2290832ceb4301418379a346328f6614606
americanomin <americanomin@gmail.com>
2022-07-31 00:56:34 +0900

    [feature] 공통 모듈 이름 변경
    [feature] 공통 모듈 이름 변경
    
    [feature] 공통 모듈 이름 변경
    
    [feature] 공통 모듈 이름 변경

--- fast-grill-api/src/main/java/com/fast/grill/ApiApplication.java
+++
@@ -1,9 +0,0 @@
-package com.fast.grill;
-
-@SpringBootApplication
-public class FastGrillApiApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ZimshopApplication.class, args);
-    }
-
-}
