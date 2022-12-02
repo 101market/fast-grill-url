fe44c34c9379d2f88b98cd5716e75df908f67c1d
americanomin <americanomin@gmail.com>
2022-12-03 00:12:57 +0900

    통계 모델 추가

---
+++ fast-grill-core/src/main/java/com/fastgrill/core/statistics/adapter/out/persistence/StatisticsRepository.java
@@ -0,0 +1,18 @@
+package com.fastgrill.core.statistics.adapter.out.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface StatisticsRepository extends JpaRepository<StatisticsJpaEntity, Integer> {
+
+    @Query(value = "SELECT new com.fastcampus.pass.repository.statistics.AggregatedStatistics(s.statisticsAt, SUM(s.hits)) " +
+            "         FROM StatisticsEntity s " +
+            "        WHERE s.statisticsAt BETWEEN :from AND :to " +
+            "     GROUP BY s.statisticsAt")
+    List<AggregatedStatistics> findByStatisticsAtBetweenAndGroupBy(@Param("from") LocalDateTime from, @Param("to") LocalDateTime to);
+
+}
